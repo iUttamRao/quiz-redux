9c6aa7d3f5aaa295de6c704f7275702449828abf
Uttam Rao <uttam.rao@nihilent.com>
2024-08-23 17:48:40 +0530

    Gasp Animation setup

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import CustomRadioButton from '../../customComponents/CustomRadioButton'
 import { Card } from 'antd';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 const Quiz = ({ quizData, selectedAnswer = [], setSelectedAnswer }) => {
+  const card = useRef()
+
+  useGSAP(() => {
+    gsap.set(card.current, { opacity: 0 });
+    gsap.to(card.current, { opacity: 1, duration: 1 });
+  },
+  { dependencies : [ quizData ] }
+);
   
   const handleChange = (e) => {
     const updatedAnswers = selectedAnswer.filter(item => item.id !== quizData?.id);
@@ -11,7 +21,7 @@ const Quiz = ({ quizData, selectedAnswer = [], setSelectedAnswer }) => {
 
   return (
     <div className='container' key={quizData?.id}>
-      <Card className='card'>
+      <Card className='card' ref={card}>
         <h2 className='question'>{quizData?.id}. {quizData?.question}</h2>
         <CustomRadioButton 
           options={quizData?.options} 
--- src/components/Quiz/QuizCounter.jsx
+++ src/components/Quiz/QuizCounter.jsx
@@ -3,22 +3,22 @@ import { Button, Pagination } from 'antd'
 import React from 'react'
 
 const QuizCounter = ({currentPage, totalPage, onChange}) => {
-  const itemRender = (_, type, originalElement) => {
-    if (type === 'prev') {
-      return <Button icon={<DoubleLeftOutlined />}>Previous</Button>;
-    }
-    if (type === 'next') {
-      return <Button icon={<DoubleRightOutlined />} iconPosition='end'>Next</Button>;
-    }
-    return originalElement;
-  };
+  // const itemRender = (_, type, originalElement) => {
+  //   if (type === 'prev') {
+  //     return <Button icon={<DoubleLeftOutlined />}>Previous</Button>;
+  //   }
+  //   if (type === 'next') {
+  //     return <Button icon={<DoubleRightOutlined />} iconPosition='end'>Next</Button>;
+  //   }
+  //   return originalElement;
+  // };
   return (
     <Pagination
       align="center"
       current={currentPage}
       total={totalPage}
       onChange={onChange}
-      itemRender={itemRender}
+      // itemRender={itemRender}
       pageSize={1}
     />
   )
